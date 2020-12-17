0ffa74cbdbb3e2ad59d786a6be1c9e02e2cdedde
huddar26 <76164848+huddar26@users.noreply.github.com>
2020-12-17 18:25:20 +0530

    Add files via upload

---
+++ randusrname.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+echo " ######################################"
+echo " # [caution] Run this script in root ##"
+echo " ######################################"
+
+
+function check_user() {
+	check1=$(whoami)
+	expec="root"
+	if [ $check1 == $expec ]
+	then
+		echo "[+] Detected root "
+		creator
+	else
+		echo "[-] FAILED ! hello user $check1"
+	fi
+
+}
+
+
+function creator() {
+	echo " [ + ] This script will create a user with strong password "	
+	read -p " Enter a path to save username associated to passwords I.E /home/username/Desktop/example.txt ==>  " path 
+	read -p" Enter the number of users you want to create " user_num
+	user_num2=$user_num
+	index=0
+	read -p " Enter the length of the password " pass_len
+	while [ $index -lt $user_num2 ] 
+	do
+		read -p " Enter a user name here ==> " user_name
+		password=$(openssl rand -base64 $pass_len)
+		sudo useradd -p $password $user_name
+		echo " $user_name ===> $password " >> $path
+		index=$((index + 1))
+	done
+}
+
+
+check_user
